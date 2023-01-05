c061288422702ad98e55555941515d862fbd6c81
Rodrigo Saraiva <rodrigobezerrasaraiva@gmail.com>
2023-01-05 14:14:32 -0300

    Separating the QR Code box into a different component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
   <main class="flex items-center flex-col justify-center align-middle py-36">
-    <div
-      id="container"
-      class="flex gap-y-4 items-center flex-col justify-center align-middle px-3 pb-8 w-1/5 h-2/4 border-2 ring-offset-8 rounded-3xl py-4 bg-white"
-    >
-      <img
-        src="/assets/img/image-qr-code.png"
-        class="flex rounded-xl w-72"
-        alt="QR Code"
-      />
-      <h1 class="flex text-slate-800 text-xl text-center font-extrabold">
-        Improve your front-end skills by building projects
-      </h1>
-      <h2 class="flex text-slate-400 text-sm text-center font-normal">
-        Scan the QR code to visit Frontend Mentor and take your coding skills to
-        the next level
-      </h2>
-    </div>
+    <QRCodeContainer></QRCodeContainer>
   </main>
 </template>
 
 <script>
+import QRCodeContainer from "./components/QRCodeContainer.vue";
+
 export default {
   name: "App",
+  components: {
+    QRCodeContainer,
+  },
 };
 </script>
